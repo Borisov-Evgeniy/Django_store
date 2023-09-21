f3f06458862684c6981d837b6c76bbb460b808cd
Evgeniy <evgeniy.borisov@yahoo.com>
2023-09-21 11:37:23 +0300

    add btn remove/update for cart

--- my_shop/cart/views.py
+++ my_shop/cart/views.py
@@ -11,24 +11,24 @@ def add_to_cart(request, product_id):
         product = get_object_or_404(Product, id=product_id)
         user = request.user
 
-        # Получите или создайте объект Cart для пользователя
+        # получение или создание Cart для пользователя
         cart, created = Cart.objects.get_or_create(user=request.user)
 
-        # Попробуйте найти существующий CartItem для данного продукта и корзины
+        # попытка найти существующий CartItem для данного продукта и корзины
         cart_item, created = CartItem.objects.get_or_create(product=product, user=user)
 
         if not created:
-            # Если CartItem уже существует, увеличьте его количество на 1
+            # Если CartItem уже существует, то + 1
             cart_item.quantity += 1
             cart_item.update_subtotal()
             cart_item.save()
         else:
-            # Если CartItem только что создан, установите его количество на 1
+            # Если CartItem только что создан, то установить кол-во 1
             cart_item.quantity = 1
             cart_item.update_subtotal()
             cart_item.save()
 
-        # Обновите общую сумму корзины и сохраните ее
+        # Обновить общую сумму корзины и сохраните ее
         cart.total += Decimal(str(product.price))
         cart.save()
 
@@ -62,7 +62,7 @@ def update_cart(request):
         cart = cart_item.cart
         product = cart_item.product
 
-        # Обновите количество товара в корзине
+        # Обновить количество товара в корзине
         if quantity > 0:
             cart_item.quantity = quantity
             cart_item.update_subtotal()
